89265da7054da57b9a07a99db9fcaa265db172cd
FadhilMob <fadhilsep2002@gmail.com>
2023-05-31 09:05:15 +0700

    no

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,29 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //Isi Arsip
-        ArsipModel::create([
-            'rhk_id'=> '1',
-            'tanggal' => '3-3-2023',
-            'judul_kegiatan' => 'Penyuluhan'
-        ]);
-        ArsipModel::create([
-            'rhk_id'=> '2',
-            'tanggal' => '21-3-2023',
-            'judul_kegiatan' => 'Senam'
-        ]);
-        ArsipModel::create([
-            'rhk_id'=> '1',
-            'tanggal' => '27-3-2023',
-            'judul_kegiatan' => 'Pembersihan'
-        ]);
         
-        //Isi Rhk
-        RhkModel::create([
-            'nama_rhk' => 'Tugas A'
-        ]);
-        RhkModel::create([
-            'nama_rhk' => 'Tugas B'
-        ]);
     }
 }
